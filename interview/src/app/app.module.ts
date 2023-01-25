10522edf090f26cede0a45e320e2e3699df2b08b
Matthew Kidd <mattofthekidd@users.noreply.github.com>
2023-01-25 13:07:39 -0700

    fix errors related to `ERROR NullInjectorError: R3InjectorError(AppModule)`

--- interview/src/app/app.module.ts
+++ interview/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,8 +10,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
