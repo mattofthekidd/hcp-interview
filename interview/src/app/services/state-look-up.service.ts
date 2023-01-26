5fa964d4bfe30ca3892ea0583a087e2f98991c87
Matthew Kidd <mattofthekidd@users.noreply.github.com>
2023-01-26 10:14:11 -0700

    Clean up the state look up service now that it is not in use.
    I don't want to delete it as I did put work into it and it does work. Removed console logs and added url link for the site it came from.

--- interview/src/app/services/state-look-up.service.ts
+++ interview/src/app/services/state-look-up.service.ts
@@ -8,18 +8,12 @@ import { Observable } from 'rxjs';
 export class StateLookUpService {
 
   constructor(private httpService: HttpClient) { }
-  private apiKey: string = "get your own";
-  //https://www.usps.com/business/web-tools-apis/address-information-api.htm#_Toc110511824
-  public async getStateByZipCode(zipCode: string): Promise<void> {
 
-    var xml = `<CityStateLookupRequest USERID="636NA0004353"><ZipCode ID='0'><Zip5>${zipCode}</Zip5></ZipCode></CityStateLookupRequest>`;
-    let t = "";
-    // await this.httpService.get<string>(`https://secure.shippingapis.com/ShippingAPI.dll?/API=CityStateLookup&XML=(` + xml + ')').subscribe(x => t = x)
-    await this.httpService.get<string>(`https://app.zipcodebase.com/api/v1/search?apikey=${this.apiKey}&codes=84321`).subscribe(x => t = x)
-    console.log(t)
+  private apiKey: string = "get your own";
+  
+  // API from here.
+  // https://zipcodebase.com/
+  public async getStateByZipCode(zipCode: string): Promise<Observable<string>> {
+    return await this.httpService.get<string>(`https://app.zipcodebase.com/api/v1/search?apikey=${this.apiKey}&codes=${zipCode}`);
   }
 }
-
-
-// Your Username is 636NA0004353
-// Your Password is 434NL56VJ000
