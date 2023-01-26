a82098eb42526a175f3e813ceb52cbf2be030024
Matthew Kidd <mattofthekidd@users.noreply.github.com>
2023-01-26 10:51:42 -0700

    Clean up and formatting for main component. Remove comments and trying to make the file more readable.
    I have left commented lines for the state look up service as it does work and I wanted to show how I had it implemented.

--- interview/src/app/app.component.ts
+++ interview/src/app/app.component.ts
@@ -15,7 +15,7 @@ import { StateLookUpService } from './services/state-look-up.service';
 export class AppComponent {
   private jsonPlaceHolderService: JsonPlaceHolderService;
   private hcpService: HCPService;
-  private stateLookUpService: StateLookUpService;
+  // private stateLookUpService: StateLookUpService;
   private subscriptions: Subscription[] = [];
 
   public users: User[] = [];
@@ -25,11 +25,11 @@ export class AppComponent {
   public constructor(
       jsonPlaceHolderService: JsonPlaceHolderService,
       hcpService: HCPService,
-      stateLookUpService: StateLookUpService,
+      // stateLookUpService: StateLookUpService,
     ) {
     this.jsonPlaceHolderService = jsonPlaceHolderService;
     this.hcpService = hcpService;
-    this.stateLookUpService = stateLookUpService;
+    // this.stateLookUpService = stateLookUpService;
   };
 
 
@@ -51,7 +51,7 @@ export class AppComponent {
       })
     );
   }
-  /*
+  /* For reference
   "first_name": "StringValue",
   "last_name": "StringValue",
   "company_name": "StringValue",
@@ -60,41 +60,33 @@ export class AppComponent {
   "phone": "2083567880"
   */
   private transformData(): void {
+
     this.users.forEach(user => {
       const name = user.name.split(" ");
       const address = user.address;
-      let phone = user.phone.split("x");
-      let splitPhone = phone[0];
-      let completePhone = splitPhone.replace(/([-.() ])/g, "");
+
+      let phone = user.phone.split("x")[0].toString(); //This removes an extension
+      let updatedPhone = phone.replace(/([-.() ])/g, "");
+      if(updatedPhone.length > 10 && updatedPhone[0] == "1") {
+        updatedPhone = updatedPhone.substring(1,11);
+      }
+
       // let state = this.stateLookUpService.getStateByZipCode(address.zipcode.split("-",1).toString());
 
       let stringUserList: string = `
-      "first_name": ${name[0]},
-      "last_name": ${name[1]},
-      "company_name": ${user.company.name},
-      "company_full_address": ${address.street}, ${address.city}, STATE, ${address.zipcode},
-      "website": ${user.website},
-      "phone": ${parseInt(completePhone)},
+      "first_name": "${name[0]}",
+      "last_name": "${name[1]}",
+      "company_name": "${user.company.name}",
+      "company_full_address": "${address.street}, ${address.city}, STATE, ${address.zipcode}",
+      "website": "${user.website}",
+      "phone": "${updatedPhone}",
       `;
+
       this.formattedUsersString += `{${stringUserList}},`;
-      // let transformedUser: UserFormatted = {
-      //   first_name: name[0],
-      //   last_name: name[1],
-      //   company_name: user.company.name,
-      //   company_full_address: `${address.street}, ${address.city}, STATE, ${address.zipcode}`,
-      //   website: user.website,
-      //   phone: parseInt(completePhone),
-      // };
-      // this.formattedUsers.push(transformedUser);
-    })
-
-    //Assuming that we were going to use this data on the site I would leave this object and create a new one for sending to the API.
-
+    });
   }
 
   private sendData(): void {
-    // console.log(this.formattedUsers)
-
     this.hcpService.postUserList(this.formattedUsersString);
   }
 
